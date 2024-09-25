a8843ee413eb90ac49f1649940a209a0adbd511a
trihag15 <trihag15@oygardenskule.no>
2024-09-25 13:01:48 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello!")
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
